c6c688fe6e61c0fec410c34fe4c59113e4429fd2
Steven Mandel <smandel@enlivant.com>
2019-08-01 14:53:24 -0500

    create the gig model

--- MoshGigHub3/Models/IdentityModels.cs
+++ MoshGigHub3/Models/IdentityModels.cs
@@ -6,20 +6,13 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MoshGigHub3.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
+
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
