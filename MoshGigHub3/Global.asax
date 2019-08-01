fd4e1b6db1996edc4733270607cf35375a3d5487
Steven Mandel <smandel@enlivant.com>
2019-08-01 14:05:15 -0500

    Add project files.

---
+++ MoshGigHub3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoshGigHub3.MvcApplication" Language="C#" %>
---
+++ MoshGigHub3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoshGigHub3.Startup))]
+namespace MoshGigHub3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
