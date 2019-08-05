5a260b6d81cee1b60b963be155af96bee57f2cf1
Steven Mandel <smandel@enlivant.com>
2019-08-05 13:23:49 -0500

    Create Add A Gig Form

---
+++ MoshGigHub3/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using MoshGigHub3.Models;
+using MoshGigHub3.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MoshGigHub3.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.OrderBy(g => g.Name).ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MoshGigHub3/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MoshGigHub3.Models;
+using System.Collections.Generic;
+
+namespace MoshGigHub3.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+
+       
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
