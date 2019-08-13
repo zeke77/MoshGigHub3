0f478e3ae087ad0140bb00d413cd8aea79fd2139
Steven Mandel <smandel@enlivant.com>
2019-08-13 13:23:55 -0500

    Implement anti forgery token

--- MoshGigHub3/Controllers/GigsController.cs
+++ MoshGigHub3/Controllers/GigsController.cs
@@ -30,6 +30,7 @@ namespace MoshGigHub3.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
 
